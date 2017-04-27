f43bfe7e3cc1038db68543e5e047dc9f71b34f33
tjramos04 <tj.ramos04@gmail.com>
2017-04-27 18:26:53 +0800

    testing

---
+++ application/controllers/Posts.php
@@ -0,0 +1,25 @@
+<?php
+
+	class Posts extends CI_Controller{
+
+		public function view($page = 'home'){
+			if(!file_exists(APPPATH.'views/pages/'.$page.'.php')){
+				show_404();
+			}
+
+			$data['title'] = ucfirst($page);
+
+			$this->load->view('templates/header');
+			$this->load->view('pages/'.$page, $data);
+			$this->load->view('templates/footer');
+		}
+
+		public function index(){
+			$data['title'] = 'LatestPost';
+
+			$this->load->view('templates/header');
+			$this->load->view('post/index', $data);
+			$this->load->view('templates/footer');
+
+		}
+	}
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -13,6 +13,7 @@
    	 <ul class="nav navbar-nav">
      	 <li ><a href="<?php echo base_url();?>">Home</a></li>
      	 <li ><a href="<?php echo base_url();?>about">About</a></li>
+     	 <li ><a href="<?php echo base_url();?>post">Post</a></li>
    	 </ul>
   </div>
 </nav>
